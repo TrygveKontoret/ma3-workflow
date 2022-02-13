8268381be24f5d161eec20ce391cb09d2f1e5c14
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2022-02-13 21:59:41 +0100

    små endringer

--- src/App.js
+++ src/App.js
@@ -3,25 +3,6 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { url } from './utils/api';
 
-// function App() {
-//   const [data, setData] = useState([]);
-    
-//   useEffect(() => {
-//      axios
-//       .get(url)
-//       .then((response) => setData(response.data.data));
-//   },[]);
-
-//   console.log(data)
-//   return (
-//     <div className='App'>
-//       <h1>heiheihehiehie</h1>
-      
-//         {data.map(penis => {
-//           <h1>{penis.attributes.Name}</h1>
-//         })}
-//     </div>
-//   )}
 
 function App() {
   const [data, setData] = useState([]);
@@ -35,8 +16,8 @@ function App() {
       <h1>Hei</h1>
       <ul>
         {data.length > 0
-          ? data.map((champion, idx) => {
-              return <li key={idx}>{champion.attributes.Name}</li>;
+          ? data.map((keiser, idx) => {
+              return <li key={idx}>{keiser.attributes.Name}</li>;
             })
           : null}
       </ul>
